acc2f03bba4b4e94ea162d1d9861b415391706cf
ashrut saxena <ashrut.saxena_cs18@gla.ac.in>
2021-08-30 18:22:30 +0530

    here this function is used for italics

---
+++ script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    let cellContainer = $(".input-cell-container");
+    for (let i = 1; i <= 100; i++) {
+        let ans = "";
+        let n = i;
+        while (n > 0) {
+            let rem = n % 26;
+            if (rem == 0) {
+                ans = "Z" + ans;
+                n = Math.floor(n / 26) - 1;
+            }
+            else {
+                ans = String.fromCharCode(rem - 1 + 65) + ans;
+                n = Math.floor(n / 26);
+            }
+        }
+
+        let column = $(`<div class="column-name colId-${i}" id="colCod-${ans}">${ans}</div>`)
+        $(".column-name-container").append(column);
+        let row = $(`<div class="row-name" id="rowid-${i}">${i}</div>`);
+        $(".row-name-container").append(row);
+    }
+    for (let i = 1; i <= 100; i++) {
+        let row = $(`<div class="cell-row"></div>`);
+        for (let j = 1; j <= 100; j++) {
+            let colcode = $(`.colId-${j}`).attr("id").split("-")[1];
+            let column = $(`<div class="input-cell" contenteditable="false" id="row-${i}-col-${j}" data="code-${colcode}"></div>`);
+            row.append(column);
+        }
+        $(".input-cell-container").append(row);
+    }
+    $(".align-icon").click(function () {
+        $(".align-icon.selected").removeClass("selected");
+        $(this).addClass("selected");
+    });
+    $(".style-icon").click(function () {
+        $(this).toggleClass("selected");
+    });
+    $(".input-cell").click(function (e) {
+        console.log(e);
+        if (e.ctrlKey) {
+            let [rowId, colId] = getrowcol(this);
+            if (rowId > 1) {
+                let topCellSelected = $(`#row-${rowId - 1}-col-${colId}`).hasClass("selected");
+                if (topCellSelected) {
+                    $(this).addClass("top-cell-selected");
+                    $(`#row-${rowId - 1}-col-${colId}`).addClass("bottom-cell-selected");
+                }
+            }
+            if (rowId < 150) {
+                let bottomCellSelected = $(`#row-${rowId + 1}-col-${colId}`).hasClass("selected");
+                if (bottomCellSelected) {
+                    $(this).addClass("bottom-cell-selected");
+                    $(`#row-${rowId + 1}-col-${colId}`).addClass("top-cell-selected");
+                }
+            }
+            if (colId > 1) {
+                let leftCellSelected = $(`#row-${rowId}-col-${colId - 1}`).hasClass("selected");
+                if (leftCellSelected) {
+                    $(this).addClass("left-cell-selected");
